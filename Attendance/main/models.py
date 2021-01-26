212843e84d263627abb1792b4d3c61ea1d3c8832
mseo.39 <a01063294905@gmail.com>
2021-01-27 05:52:48 +0900

    graph

--- Attendance/main/models.py
+++ Attendance/main/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class Member(models.Model): #모델명의 첫글자는 대문자로
     name = models.CharField(max_length = 50) #최대로 넣을 수 있는 글자 수
     student_id = models.CharField(max_length = 50)
+    major = models.CharField(max_length = 50, null=True)
+    attendance = models.IntegerField(default=0)
+    absent = models.IntegerField(default=0)
+    tardy = models.IntegerField(default=0)
+    etc = models.IntegerField(default=0)
 
     def __str__(self): #제목에 오브젝트가 아니라 이름이 나오도록
         return self.name
--- Attendance/main/views.py
+++ Attendance/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 import sqlite3
 from main.models  import Member, Attendance #모델의 존재 알려주기
+import matplotlib.pyplot as plt
+from io import StringIO
+import numpy as np
 
 # Create your views here.
 
@@ -19,9 +22,24 @@ def chk(request):
     attendance.attendance=request.POST['attendance']
     attendance.date=request.POST['date']
     names=Member.objects.all()
+    name=request.POST['name']
+
+    member_info=get_object_or_404(Member, name=name)
+    if attendance.attendance=="출석":
+        member_info.attendance+=1
     
+    elif attendance.attendance=="결석":
+        member_info.absent+=1
+    
+    elif attendance.attendance=="지각":
+        member_info.tardy+=1
+
+    elif attendance.attendance=="기타":
+        member_info.etc+=1
+
+    member1.save()
     attendance.save()
-    
+
     return render(request,'main.html',{'date':attendance.date,'names':names})
 
 def date(request):
@@ -47,4 +65,27 @@ def detail(request, name_id):
     names=Member.objects.all()
     name=get_object_or_404(Member, pk=name_id)
     member_name = Attendance.objects.order_by('-date').filter(name__contains='{}'.format(name.name))
-    return render(request, 'member.html', {'names':names,'member_name':member_name})
+
+    data = return_graph(name_id)
+
+    return render(request, 'member.html', {'names':names,'member_name':member_name,'data':data})
+
+def return_graph(name_id):
+
+    name=get_object_or_404(Member, pk=name_id)
+   
+    fig = plt.figure()
+
+    x=np.arange(4) #주어진 범위와 간격에 따라 균일한 값을 갖는 어레이를 생성하는 함수
+    valuetype=['attendance','absent','tardy','etc'] #x축에 표시될
+    values=[name.attendance, name.absent, name.tardy, name.etc] #막대 그래프의 높이로 표시될 y 값 
+    plt.title("attendance")
+    plt.bar(x,values)
+    plt.xticks(x, valuetype)
+
+    imgdata = StringIO()
+    fig.savefig(imgdata, format='svg')
+    imgdata.seek(0)
+
+    data = imgdata.getvalue()
+    return data
